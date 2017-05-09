6577e19372fe6edf929810fde17b0cb8346e0563
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-09 10:03:53 +0530

    Local storage functionality implemented

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 	$('.login').on('click',function(){
 		$('section').hide();
-		$('.login_page, .back_link').show();
+		$('.login_page,.back_link').show();
 	});
 
 	$('.signup').on('click',function(){
@@ -12,8 +12,47 @@ $(document).ready(function(){
 		$('.signup_page,.back_link').show();
 	});
 
-	$('.back_link').on('click',function(){
+	$('.back_link, .logout').on('click',function(){
 		$('section').hide();
 		$('.main_page').show();
+	});
+
+	$('.sign_submit').on('click', function(e){
+		e.preventDefault();
+		var userName = $("#txtSignName").val();
+		var userEmail = $("#txtEmail").val();
+		var userPwd = $("#txtSignPassword").val();
+
+		if(typeof Storage !== 'undefined') {
+			localStorage.setItem('uname', userName);
+			localStorage.setItem('uemail', userEmail);
+			localStorage.setItem('upwd', userPwd);
+		}
+		else {
+			alert("Your browser does not support local storages.");
+		}
+		
+		$('section').hide();
+		$('.main_page').show();
+	});
+	$('.login_submit').on('click', function(e){
+		e.preventDefault();
+
+		$('section').hide();
+		$('.home_page').show();
+
+		var signName = localStorage.getItem('uname');
+		var signEmail = localStorage.getItem('uemail');
+		var signPwd = localStorage.getItem('upwd');
+
+		var logName = $('#txtUsername').val();
+		var logpwd = $('#txtpassword').val();
+
+		if((logName == signName) && (logpwd == signPwd)) {
+			$('#welcomeUser').html('Welcome to ' + logName);
+		}
+		else {
+			$('#welcomeUser').html('User name or Password is incorrect'); 
+		}
 	});
 });
